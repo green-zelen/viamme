f5712a3b381ca750a3931909045833f3eae3f329
Мария Перловская <green@192.168.1.65>
2019-02-18 16:35:37 +0100

    Настройка проекта на gulp.

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# viamme
-viamme app web-site first design
+VIAMME Style
+======================
+
+Верстка проекта. HTMl&CSS&JS
+
+## Требования
+* Node.js
+
+## Инструменты
+* HTML5
+* CSS3
+* PostCss
+
+
+## Как начать работу:
+
+```
+https://github.com/green-zelen/viamme.git
+глобально:
+~ sudo npm install gulp -g
+
+или локально:
+~ cd viamme
+~ npm install --save-dev gulp
+```
+
+## Как установить плагины PostCss*
+
+```
+~ npm install --save-dev gulp-postcss
+~ npm install --save-dev gulp-autoprefixer
+~ npm install --save-dev gulp-concat
+~ npm install --save-dev precss
+~ npm install --save-dev postcss-scss
+~ npm install --save-dev postcss-nested
+~ npm install --save-dev short
+~ npm install --save-dev postcss-sorting
+~ npm install --save-dev cssnano
+~ npm install --save-dev postcss-strip-inline-comments
+~ npm install -save-dev postcss-short
+~ npm install --save-dev gulp-csslint
+~ npm i gulp-pug
+
+```
+## Структура директорий
+
+* Исполняемый код /public
+* Исходный код /src
+* Компоненты /src/project/*/*
+* Переменные /src/variables
+
+## Как компилировать HTML:
+
+```
+~ gulp html
+```
+
+## Как компилировать CSS3:
+
+```
+~ gulp css
+```
+
+## Как компилировать JS:
+
+```
+~ gulp js
+```
+
+## Запустить Linter:
+
+```
+~ gulp lint
+```
---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+var gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+var postcss = require('gulp-postcss');
+var stripInlineComments = require('postcss-strip-inline-comments');
+var scss = require('postcss-scss');
+var concat = require('gulp-concat');
+var nested = require('postcss-nested');
+var precss = require('precss');
+var short = require('postcss-short');
+var sorting = require('postcss-sorting');
+var csslint = require('gulp-csslint');
+var pug = require('gulp-pug');
+
+
+// Сборка компонентов в полные файлы
+
+gulp.task('com', function() {
+    return gulp.src('./src/scss/common.blocks/*/*.scss')
+        .pipe(autoprefixer({
+            browsers: ['> 0%', 'IE 9'],
+            cascade: false
+        }))
+        .pipe(concat('project.css'))
+        .pipe(gulp.dest('./src/'));
+});
+
+gulp.task('resp', function() {
+    return gulp.src('./src/scss/responsive.blocks/*/*/*.scss')
+        .pipe(concat('project-responsive.css'))
+        .pipe(gulp.dest('./src/'));
+});
+
+gulp.task('js-def', function() {
+    return gulp.src('./src/scss/*/*/*.js')
+        .pipe(concat('project.js'))
+        .pipe(gulp.dest('./public/javascript'));
+});
+
+gulp.task('js-resp', function() {
+    return gulp.src('./src/scss/*/*/*/*.js')
+        .pipe(concat('project-responsive.js'))
+        .pipe(gulp.dest('./public/javascript'));
+});
+
+
+// Компиляция CSS
+
+gulp.task('postcss', function() {
+    var preprocessors = [
+        precss,
+    ]
+
+    var processors = [
+        nested,
+        short,
+        sorting,
+        stripInlineComments
+    ];
+    return gulp.src('./src/*.css')
+        .pipe(postcss(preprocessors))
+        .pipe(postcss(processors, { syntax: scss }))
+        .pipe(gulp.dest('./public/css'));
+});
+
+// Вызов всех команд
+gulp.task('css', ['com', 'resp', 'postcss']);
+gulp.task('js', ['js-def', 'js-resp']);
+
+// Компиляция html
+
+gulp.task('html', function buildHTML() {
+  return gulp.src('./src/pug/*.pug')
+  .pipe(pug())
+  .pipe(gulp.dest('./public'));
+});
+
+// CSS Линтер
+
+gulp.task('lint', function() {
+    gulp.src('./public/css/*.css')
+        .pipe(csslint())
+        .pipe(csslint.reporter());
+});
